754692349c6958dc062e4b20dfda73aad524639b
GCA\jlarrubi <jlarrubia@inscaparrella.cat>
2023-03-02 17:22:11 +0100

    Added student postman files and html reports closes #6

---
+++ Startup.bat
@@ -0,0 +1,2 @@
+C:\Users\78082108Y\Documents\Programs\node-v18.14.2-win-x64\newman run "MongoDB Data API.postman_collection.json" -e "Data API.postman_environment.json" -r htmlextra
+C:\Users\78082108Y\Documents\Programs\node-v18.14.2-win-x64\newman run "NttData-student-gencat.postman_collection.json" -e "NttData-Student_PRO.postman_environment.json" -r htmlextra
